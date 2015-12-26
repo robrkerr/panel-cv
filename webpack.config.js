809c17f0aa2cea2805b125db9d936f7a8bf6e436
Robert Kerr <robrkerr@gmail.com>
2015-12-26 12:54:43 +1000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+      { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer-core'),
+    require('postcss-color-rebeccapurple')
+  ],
+
+  resolve: {
+    modulesDirectories: ['node_modules']
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true })
+  ]
+};
